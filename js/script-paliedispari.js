f288ff387f9b99b8efaec5e69b39358a7e758376
lucanovelli.boolean <luca.novelli.lavoro@gmail.com>
2024-05-20 16:12:23 +0200

    add: modificated regular expression, it's accepted only words and spaces now. Added method to check if the string contains space or not

--- js/script-paliedispari.js
+++ js/script-paliedispari.js
@@ -4,10 +4,26 @@
 const input = prompt("Inserisci la parola");
 
 //REGULAR EXPRESSION, NON ACCETTI NUMERI E CARATTERI SPECIALI
-const regex = /^[a-zA-Z]+$/;
+const regex = /^\w+(?: \w+)*$/;
 
 //CONTROLLO CHE SIA UNA STRINGA E NON UN NUMERO
 if (regex.exec(input) != null) {
-    const array = [];
-    array.push(input);
+    const string_input = input;
+    if (string_input.indexOf(" ") !== -1) {
+        console.log("CONTIENE SPAZI");
+        const array_reverse = input.split(" ").reverse().join("");
+        console.log("STRINGA ----> " + input + " // STRINGA REVERSE ---> " + array_reverse);
+    }
+    else {
+        console.log("NON CONTIENE SPAZI");
+        const array_reverse = input.split("").reverse().join("");
+        console.log("STRINGA ----> " + input + " // STRINGA REVERSE ---> " + array_reverse);
+    }
 }
+
+//METODO CONTROLLO CHE LA STRINGA SIA PALINDROMA
+/*function checkPalindrome(stringa_uno, stringa_due) {
+    
+    if[array === 
+
+}*/
