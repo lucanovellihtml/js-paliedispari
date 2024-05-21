3c9211b7f33a7dd4cd47bea25aff388d06d294c0
lucanovelli.boolean <luca.novelli.lavoro@gmail.com>
2024-05-21 17:29:26 +0200

    add: added the method to check if the sum is even or odd

--- pari-dispari/js/script-pari-dispari.js
+++ pari-dispari/js/script-pari-dispari.js
@@ -4,10 +4,32 @@
 const button_even = document.getElementById("button_even");
 const button_odd = document.getElementById("button_odd");
 
+//METODO DI ASCOLTO DEL CLICK SUL PULSTANTE PARI O DISPARI
 button_even.addEventListener("click", function () {
     console.log("HAI PREMUTO PARI");
+    checkSumm(true);
 });
 
 button_odd.addEventListener("click", function () {
     console.log("HAI PREMUTO DISPARI");
+    checkSumm(false);
 });
+
+
+//METODO CHE CONTROLLA SE E' PARI O DISPARI
+function checkSumm(flag) {
+
+    const number = prompt("Inserisci un numero tra 1 e 5");
+    const summ = parseInt(number) + Math.floor(Math.random() * 5) + 1;
+    console.log("SOMMA ---> " + summ);
+
+    if (summ % 2 === 0 && flag)
+        console.log("HAI VINTO, IL NUMERO E' PARI");
+
+    else if (summ % 2 !== 0 && !flag)
+        console.log("HAI VINTO, IL NUMERO E' DISPARI");
+
+    else
+        console.log("HAI PERSO");
+
+}
